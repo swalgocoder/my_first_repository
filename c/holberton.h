0dd7551db3c0db410b29767ed528c7db6fd0b8f1
swalgocoder <gary.d.gao@gmail.com>
2016-11-22 14:09:25 -0800

    Create holberton.h

---
+++ c/holberton.h
@@ -0,0 +1,24 @@
+#ifndef H_98_H
+#define HO_98_H
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ *struct print_a - A struct that has a pointer character and a pointer
+ *to a function
+ *@s: a character pointer
+ *@f: a function pointer
+ */
+typedef struct data_type
+{
+	char *check_char;
+	int *(*f)();
+} my_type;
+
+int my_format(va_list args, my_type data_type[], char format);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int my_number(int n);
+void my_struct(void);
+int my_percentage(int p);
+void _puts(char *str);
+#endif
